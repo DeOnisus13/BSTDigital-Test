3705c7a5b783b8cb8e874e2c118dbd1833b86a1a
Denis Antropov <de94onisus@gmail.com>
2024-12-17 12:16:26 +0400

    Task 2

--- robots/urls.py
+++ robots/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from robots.apps import RobotsConfig
-from robots.views import RobotCreateView, RobotBulkCreateView, IndexView
+from robots.views import RobotCreateView, RobotBulkCreateView, IndexView, ExcelReportView, export_to_excel
 
 app_name = RobotsConfig.name
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='main_page'),
     path('add_robot/', RobotCreateView.as_view(), name='add_robot'),
     path('add_robots/', RobotBulkCreateView.as_view(), name='add_robots'),
+    path('get_report/', ExcelReportView.as_view(), name='get_report'),
+    path('export_to_excel/', export_to_excel, name='export_to_excel'),
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,7 +1,13 @@
 import json
-from django.http import JsonResponse
+from datetime import timedelta, datetime
+
+from django.http import JsonResponse, HttpResponse
+from django.utils.timezone import now, make_aware
 from django.views.generic import TemplateView, CreateView
 from django.core.exceptions import ValidationError
+from openpyxl.workbook import Workbook
+
+from robots.forms import DateForm
 from robots.models import Robot
 
 
@@ -75,3 +81,69 @@ class RobotBulkCreateView(CreateView):
             })
         except json.JSONDecodeError as err:
             return JsonResponse({"error": f"Invalid JSON format: {str(err)}"}, status=400)
+
+
+class ExcelReportView(TemplateView):
+    """
+    Отображение страницы для ввода даты и получения отчета в excel-файл.
+    """
+    form_class = DateForm
+    template_name = "robots/excel_report_form.html"
+
+    def get_context_data(self, **kwargs):
+        """Переопределение метода для отображения нужной формы."""
+        context = super().get_context_data(**kwargs)
+        context["form"] = self.form_class()
+        return context
+
+
+def export_to_excel(request):
+    """
+    Функция для получения данных из формы, фильтрации данных в БД и создания excel-файла.
+    """
+    # Обработка формы
+    form = DateForm(request.GET or None)
+    if form.is_valid():
+        end_date = form.cleaned_data['date']
+    else:
+        end_date = now().date()
+
+    # Расчет начала периода
+    start_date = end_date - timedelta(days=7)
+    start_datetime = make_aware(datetime.combine(start_date, datetime.min.time()))
+    end_datetime = make_aware(datetime.combine(end_date, datetime.max.time()))
+
+    # Фильтрация объектов за последнюю неделю
+    robots = Robot.objects.filter(created__range=(start_datetime, end_datetime))
+
+    # Группировка по модели и версии
+    grouped_data = {}
+    for robot in robots:
+        if robot.model not in grouped_data:
+            grouped_data[robot.model] = {}
+        if robot.version not in grouped_data[robot.model]:
+            grouped_data[robot.model][robot.version] = 0
+        grouped_data[robot.model][robot.version] += 1
+
+    if not grouped_data:
+        return JsonResponse({"message": "Нет данных за указанный период"}, status=404)
+
+    # Создание Excel-файла
+    wb = Workbook()
+    for model, versions in grouped_data.items():
+        ws = wb.create_sheet(title=model)
+        ws.append(['Модель', 'Версия', 'Количество за неделю'])
+        for version, count in versions.items():
+            ws.append([model, version, count])
+
+    # Удаляем стандартный пустой лист, если он существует
+    if 'Sheet' in wb.sheetnames:
+        wb.remove(wb['Sheet'])
+
+    # Создание HTTP-ответа с Excel-файлом
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = f'attachment; filename=robots_{end_date}.xlsx'
+    wb.save(response)
+    return response
